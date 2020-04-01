aaa2a065eddf9522ba7b0dec3bf058e353d8d947
Anna Gordon <annetta@email.com>
2020-04-01 21:59:46 +0000

    table on admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @categories = Category.count
+    @products = Product.count
+    # puts @categories.name
+    # raise @products.inspect
   end
 end
